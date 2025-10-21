d6764752807ee007e26e6521564a35a63e635a95
Yi-Ting1205 <111960555+Yi-Ting1205@users.noreply.github.com>
2025-10-22 02:08:46 +0800

    Update Server.ps

--- Server.py
+++ Server.py
@@ -214,7 +214,9 @@ async def health_check():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("Server:app", host="0.0.0.0", port=port, workers=1, timeout_keep_alive=60)
+
 
 # from fastapi import FastAPI, UploadFile, File
 # from fastapi.responses import JSONResponse
